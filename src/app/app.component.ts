38ee369a87eea604b8dabce0a656346128421767
ibrahim <ibragimiminov7@gmail.com>
2025-06-26 11:43:36 +0300

    Revorked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculatorComponent],
+  imports: [CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://s1.divlop.com:5001',
+  useProxy: false
+}; 
